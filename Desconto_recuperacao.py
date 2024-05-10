9cb67bd206f478b1bade95cfe65342953c41df4d
Darrandaran <132006096+Darrandaran@users.noreply.github.com>
2024-05-10 11:20:25 -0300

    Update Desconto_recuperacao.py

--- Desconto_recuperacao.py
+++ Desconto_recuperacao.py
@@ -1,12 +1,23 @@
 #definindo tupla com produtos e seus valores
 
 produtos = (
- 'tab' ("produtos1" = 10),
- 'tab' ("produtos2" = 20),
- 'tab' ("produtos3" = 30),
- 'tab' ("produtos4" = 40),
- 'tab' ("produtos5" = 50)
+    ("produtos1", 10),
+    ("produtos2", 20),
+    ("produtos3", 30),
+    ("produtos4", 40),
+    ("produtos5", 50)
 )
 
 #calcule a soma dos valores do produto
-soma = sum(valor for_,)
+soma = sum(valor for _, valor in produtos)
+
+#verificando se a soma é maior q 200 e aplicando o desconto
+if soma > 200:
+    desconto = soma * 0.10 #desconto 10%
+elif soma <= 200:
+    desconto = soma * 0.05 #desconto 5%
+
+#aplicando desconto se necessário
+soma -= desconto
+
+print("o valor total dos produtos é:", soma)
