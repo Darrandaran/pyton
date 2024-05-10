5c21d666a977f080e0007d572e76d8ce7eef79bc
Darrandaran <132006096+Darrandaran@users.noreply.github.com>
2024-05-10 10:56:53 -0300

    Create Desconto_recuperacao.py

---
+++ Desconto_recuperacao.py
@@ -0,0 +1,12 @@
+#definindo tupla com produtos e seus valores
+
+produtos = (
+ 'tab' ("produtos1" = 10),
+ 'tab' ("produtos2" = 20),
+ 'tab' ("produtos3" = 30),
+ 'tab' ("produtos4" = 40),
+ 'tab' ("produtos5" = 50)
+)
+
+#calcule a soma dos valores do produto
+soma = sum(valor for_,)
